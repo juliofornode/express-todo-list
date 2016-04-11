851b5738651bc9d5207fccc90eef54d4a5e805fa
Julio Colomer <juliocolomer@Julios-MacBook-Air.local>
2016-04-10 17:49:35 -0700

    initial setup

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-//1. app dependencies
+//1. app dependencies: npm and our own modules
+var express = require('express');
+
 
 //2. db connection
 
+
 //3. app instantiation
+var app = express();
 
-//4. app configuration
 
-//5. middleware definition
+//4. app configuration (views, views engine, etc)
+app.set('views', _dirname + '/views');
+app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
+var port = app.get('port');
+
+
+//5. middleware definition (static, form handling, error handling, etc)
+app.use(express.static(__dirname + '/public'));
+
 
 //6. routes and request handlers
 
+
 //7. app server start with host and port
+app.listen(port, function() {
+   console.log('the server is listening on port ', port);
+});
