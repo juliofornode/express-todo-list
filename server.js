0f0d08d4cd14822c9788671d24923e0cb57db8fb
Julio Colomer <juliocolomer@Julios-MacBook-Air.local>
2016-04-10 19:07:17 -0700

    added logger, disabled x-powered-by, set index route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 //npm modules
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
 
 
 //our own modules
@@ -17,18 +18,20 @@ var app = express();
 
 
 //4. app configuration (views, views engine, etc)
-app.set('views', _dirname + '/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
 var port = app.get('port');
+app.set('x-powered-by', false);
 
 
 //5. middleware definition (static, form handling, error handling, etc)
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(logger('dev'));
 
 
 //6. routes and request handlers
-
+app.get('/', routes.index);
 
 //7. app server start with host and port
 app.listen(port, function() {
