4ebb4a1fd8f5e7c26f3144a05631a36d9b3fbc74
Julio Colomer <juliocolomer@Julios-MacBook-Air.local>
2016-04-10 17:42:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Simple todo list with ExpressJS and Mongoskin.
---
+++ server.js
@@ -0,0 +1,13 @@
+//1. app dependencies
+
+//2. db connection
+
+//3. app instantiation
+
+//4. app configuration
+
+//5. middleware definition
+
+//6. routes and request handlers
+
+//7. app server start with host and port
