260d233020c659f0c1c2673a68da5ade100741f5
Julio Colomer <juliocolomer@Julios-MacBook-Air.local>
2016-04-11 10:28:09 -0700

    added mongodb and method-override modules

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 
 //npm modules
 var express = require('express');
+//path.join normalizes the url (unix and window)
 var path = require('path');
 var logger = require('morgan');
+var mongoskin = require('mongoskin');
+//method-override allows us to use http verbs with old browsers
+var methodOverride = require('method-override');
 
 
 //our own modules
@@ -11,6 +15,7 @@ var routes = require('./routes');
 
 
 //2. db connection
+var db = mongoskin.db('mongodb://localhost:27017/mytodo?auto_reconnect', {safe:true});
 
 
 //3. app instantiation
@@ -28,10 +33,17 @@ app.set('x-powered-by', false);
 //5. middleware definition (static, form handling, error handling, etc)
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(logger('dev'));
+app.use(methodOverride());
+app.use(function(req, res, next) {
+   req.db = {};
+   req.db.tasks = db.collection('tasks');
+   next();
+});
 
 
 //6. routes and request handlers
 app.get('/', routes.index);
+
 
 //7. app server start with host and port
 app.listen(port, function() {
