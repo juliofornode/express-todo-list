9f8c669a6b5a3d18ff89fc05f7798b001a058b8c
Julio Colomer <juliocolomer@Julios-MacBook-Air.local>
2016-04-11 18:31:00 -0700

    added less-middleware module and configuration

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var logger = require('morgan');
 var mongoskin = require('mongoskin');
 //method-override allows us to use http verbs with old browsers
 var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var csrf = require('csurf');
+var lessMiddleware = require('less-middleware');
 
 
 //our own modules
@@ -32,6 +38,7 @@ app.set('port', process.env.PORT || 3000);
 var port = app.get('port');
 app.set('x-powered-by', false);
 app.locals.appname = 'myApp';
+app.locals.moment = require('moment');
 
 
 //5. middleware definition (static, form handling, error handling, etc)
@@ -44,11 +51,52 @@ app.use(function(req, res, next) {
    req.db.tasks = db.collection('tasks');
    next();
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser('CEAF3FA4-F385-49AA-8FE4-54766A9874F1'));
+app.use(session({
+   secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9',
+   resave: true,
+   saveUninitialized: true
+}));
+app.use(csrf());
+app.use(function(req, res, next) {
+   res.locals._csrf = req.csrfToken();
+   return next();
+});
+app.use(lessMiddleware(path.join(__dirname + '/public')));
+
+// development only
+if ('development' == app.get('env')) {
+   app.use(errorHandler());
+}
+
 
 
 //6. routes and request handlers
+app.param('task_id', function(req, res, next, taskId) {
+   req.db.tasks.findById(taskId, function(error, task){
+      if (error) return next(error);
+      if (!task) return next(new Error('Task is not found.'));
+      req.task = task;
+      return next();
+   });
+});
+
 app.get('/', routes.index);
 app.get('/tasks', tasks.list);
+app.post('/tasks', tasks.add);
+app.post('/tasks/:task_id', tasks.markCompleted);
+app.post('/tasks', tasks.markAllCompleted);
+app.delete('/tasks/:task_id', tasks.del);
+app.get('/tasks/completed', tasks.completed);
+
+
+//catch-all route
+app.all('*', function(req, res){
+   res.status(404).send();
+});
+
 
 
 //7. app server start with host and port
