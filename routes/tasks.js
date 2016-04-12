59d63a9b5d9e5de7bfcd21363cf5bcaaf52e80f5
Julio Colomer <juliocolomer@Julios-MacBook-Air.local>
2016-04-11 19:02:48 -0700

    minor changes

--- routes/tasks.js
+++ routes/tasks.js
@@ -63,7 +63,7 @@ exports.del = function(req, res, next) {
 exports.completed = function(req, res, next) {
     req.db.tasks.find({completed: true}).toArray(function(error, tasks) {
         res.render('tasks_completed', {
-            title: 'Completed',
+            title: 'completed tasks',
             tasks: tasks || []
         });
     });
